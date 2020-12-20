96b50205e4d346f24fc15826e2077289ba8d13cc
Magdalena <magdalenaouirane@gmail.com>
2020-12-20 22:58:30 +0100

    add funcionality for image-slider project

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
+const pictures = [
+  "contBcg-0",
+  "contBcg-1",
+  "contBcg-2",
+  "contBcg-3",
+  "contBcg-4",
+];
 
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+const imgContainer = document.querySelector(".img-container");
 
-  
+const buttons = document.querySelectorAll(".btn");
 
+let active = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(button);
+    if (button.classList.contains("btn-right")) {
+      active++;
+      imgContainer.style.backgroundImage = `url('./img/${pictures[active]}.jpeg')`;
+    } else if (button.classList.contains("btn-left")) {
+      active--;
+      imgContainer.style.backgroundImage = `url('./img/${pictures[active]}.jpeg')`;
+    }
+  });
+});
